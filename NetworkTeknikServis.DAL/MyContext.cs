5bfbad726ad9499b7072da122a69a10726899ef2
Oğuzhan Dereci <oguzz.dereci@gmail.com>
2019-02-13 14:47:43 +0300

    asp.net identity entegrasyonu yapıldı

---
+++ NetworkTeknikServis.DAL/MyContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using NetworkTeknikServis.MODELS.IdentityModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkTeknikServis.DAL
+{
+    public class MyContext : IdentityDbContext<User>
+    {
+        public DateTime InstanceDate { get; set; }
+        public MyContext():base("name=MyCon")
+        {
+            this.InstanceDate = DateTime.Now;
+        }
+    }
+}
---
+++ NetworkTeknikServis.MODELS/Enums/IdentityRoles.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkTeknikServis.MODELS.Enums
+{
+    public enum IdentityRoles
+    {
+        Admin,
+        Customer,
+        Operator,
+        Technician
+    }
+}
