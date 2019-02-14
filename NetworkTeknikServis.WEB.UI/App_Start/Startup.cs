86863b736ae27ba3e35c1a71d3d354d001d75166
Oğuzhan Dereci <oguzz.dereci@gmail.com>
2019-02-14 09:01:56 +0300

    owin startup

---
+++ NetworkTeknikServis.WEB.UI/App_Start/Startup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+[assembly: OwinStartup(typeof(NetworkTeknikServis.WEB.UI.App_Start.Startup))]
+
+namespace NetworkTeknikServis.WEB.UI.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account")
+            });
+        }
+    }
+}
