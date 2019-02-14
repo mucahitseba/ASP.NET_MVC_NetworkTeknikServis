cb0958e05d8a6a7645362d96d03b9e5c84a4c281
Mücahit Seba <mucahitseba@gmail.com>
2019-02-14 20:02:06 +0200

    Profil güncelleme sayfası yapıldı

---
+++ NetworkTeknikServis.MODELS/ViewModels/UserProfileViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkTeknikServis.MODELS.ViewModels
+{
+    public class UserProfileViewModel
+    {
+        public string Id { get; set; }
+        public string Email { get; set; }
+        [StringLength(50)]
+        [Required]
+        public string Name { get; set; }
+        [StringLength(60)]
+        [Required]
+        public string Surname { get; set; }
+        public string PhoneNumber { get; set; }
+        public string UserName { get; set; }
+    }
+}
--- NetworkTeknikServis.WEB.UI/Controllers/AccountController.cs
+++ NetworkTeknikServis.WEB.UI/Controllers/AccountController.cs
@@ -134,5 +134,143 @@ namespace NetworkTeknikServis.WEB.UI.Controllers
             authManager.SignOut();
             return RedirectToAction("Index","Account");
         }
+        [HttpGet]
+        [Authorize]
+        public async Task<ActionResult> UserProfile()
+        {
+            var id = HttpContext.GetOwinContext().Authentication.User.Identity.GetUserId();
+
+            try
+            {
+                var userManager = NewUserManager();
+                var user = await userManager.FindByIdAsync(id);
+
+                var data = new ProfilePasswordViewModel()
+                {
+                    UserProfileViewModel = new UserProfileViewModel()
+                    {
+                        Email = user.Email,
+                        Id = user.Id,
+                        Name = user.Name,
+                        Surname = user.Surname,
+                        PhoneNumber = user.PhoneNumber,
+                        UserName = user.UserName
+                    }
+                };
+                return View(data);
+            }
+            catch (Exception ex)
+            {
+                TempData["Model"] = new ErrorViewModel()
+                {
+                    Text = $"Bir hata oluştu {ex.Message}",
+                    ActionName = "UserProfile",
+                    ControllerName = "Account",
+                    ErrorCode = 500
+                };
+                return RedirectToAction("Error", "Home");
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<ActionResult> UpdateProfile(ProfilePasswordViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("UserProfile", model);
+            }
+
+            try
+            {
+                var userManager = NewUserManager();
+                var user = await userManager.FindByIdAsync(model.UserProfileViewModel.Id);
+
+                user.Name = model.UserProfileViewModel.Name;
+                user.Surname = model.UserProfileViewModel.Surname;
+                if (user.Email != model.UserProfileViewModel.Email)
+                {
+                    //todo tekrardan aktivasyon gönderilmeli ve kullanıcı rolü aktif olamayan bir kullanıcı rolüne atanmalı!
+                }
+                user.Email = model.UserProfileViewModel.Email;
+                user.PhoneNumber = model.UserProfileViewModel.PhoneNumber;
+                await userManager.UpdateAsync(user);
+                TempData["Message"] = "Profiliniz güncellenmiştir";
+                return RedirectToAction("UserProfile");
+            }
+            catch (Exception ex)
+            {
+                TempData["Model"] = new ErrorViewModel()
+                {
+                    Text = $"Bir hata oluştu {ex.Message}",
+                    ActionName = "UserProfile",
+                    ControllerName = "Account",
+                    ErrorCode = 500
+                };
+                return RedirectToAction("Error", "Home");
+            }
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public async Task<ActionResult> ChangePassword(ProfilePasswordViewModel model)
+        {
+            try
+            {
+                var userManager = NewUserManager();
+                var id = HttpContext.GetOwinContext().Authentication.User.Identity.GetUserId();
+                var user = NewUserManager().FindById(id);
+                var data = new ProfilePasswordViewModel()
+                {
+                    UserProfileViewModel = new UserProfileViewModel()
+                    {
+                        Email = user.Email,
+                        Id = user.Id,
+                        Name = user.Name,
+                        PhoneNumber = user.PhoneNumber,
+                        Surname = user.Surname,
+                        UserName = user.UserName
+                    }
+                };
+                model.UserProfileViewModel = data.UserProfileViewModel;
+                if (!ModelState.IsValid)
+                {
+                    return View("UserProfile", model);
+                }
+
+
+                var result = await userManager.ChangePasswordAsync(
+                    HttpContext.GetOwinContext().Authentication.User.Identity.GetUserId(),
+                    model.ChangePasswordViewModel.OldPassword, model.ChangePasswordViewModel.NewPassword);
+
+                if (result.Succeeded)
+                {
+                    //todo kullanıcıyı bilgilendiren bir mail atılır
+                    return RedirectToAction("Logout", "Account");
+                }
+                else
+                {
+                    var err = "";
+                    foreach (var resultError in result.Errors)
+                    {
+                        err += resultError + " ";
+                    }
+                    ModelState.AddModelError("", err);
+                    return View("UserProfile", model);
+                }
+            }
+            catch (Exception ex)
+            {
+                TempData["Model"] = new ErrorViewModel()
+                {
+                    Text = $"Bir hata oluştu {ex.Message}",
+                    ActionName = "UserProfile",
+                    ControllerName = "Account",
+                    ErrorCode = 500
+                };
+                return RedirectToAction("Error", "Home");
+            }
+        }
     }
 }
