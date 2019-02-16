1e3d88ef4d7e167d56c23e2dc9f5efe475df9220
Oğuzhan DERECİ <oguzz.dereci@gmail.com>
2019-02-17 02:33:06 +0300

    update

---
+++ NetworkTeknikServis.WEB.UI/Controllers/TechnicianController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NetworkTeknikServis.WEB.UI.Controllers
+{
+    [Authorize(Roles = "Technician")]
+    public class TechnicianController : Controller
+    {
+        // GET: Technician
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
