6cd30d2c5c1ecaddebec0b061814ef69eb7cbeac
Oğuzhan Dereci <oguzz.dereci@gmail.com>
2019-02-19 12:29:10 +0300

    operator arıza onay ok

---
+++ NetworkTeknikServis.MODELS/ViewModels/FaultProfileViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkTeknikServis.MODELS.ViewModels
+{
+    public class FaultProfileViewModel
+    {
+        public UserProfileViewModel UserProfileViewModel { get; set; }
+        public FaultViewModel FaultViewModel { get; set; }
+    }
+}
--- NetworkTeknikServis.WEB.UI/Controllers/OperatorController.cs
+++ NetworkTeknikServis.WEB.UI/Controllers/OperatorController.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 using NetworkTeknikServis.BLL.Repository;
+using NetworkTeknikServis.BLL.Services.Senders;
 using NetworkTeknikServis.MODELS.ViewModels;
+using static NetworkTeknikServis.BLL.Identity.MembershipTools;
 
 namespace NetworkTeknikServis.WEB.UI.Controllers
 {
@@ -30,6 +34,43 @@ namespace NetworkTeknikServis.WEB.UI.Controllers
                 TechnicianId = x.TechnicianId,
             }).ToList();
             return View(data);
+        }
+
+        public async Task<ActionResult> ConfirmFault(Guid id)
+        {
+            try
+            {
+                var fault = new FaultRepo().GetById(id);
+                var customer = await NewUserStore().FindByIdAsync(fault.CustomerId);
+                var user = await NewUserStore().FindByIdAsync(HttpContext.GetOwinContext().Authentication.User.Identity.GetUserId());
+                if (user != null)
+                {
+                    fault.AssignedOperator = true;
+                    fault.OperatorId = user.Id;
+                    new FaultRepo().Update(fault);
+
+                    string SiteUrl = Request.Url.Scheme + System.Uri.SchemeDelimiter + Request.Url.Host +
+                                     (Request.Url.IsDefaultPort ? "" : ":" + Request.Url.Port);
+
+                    var emailService = new EmailService();
+                    var body = $"Merhaba <b>{customer.Name} {customer.Surname}</b><br>Arıza talebiniz onaylanmıştır.";
+                    await emailService.SendAsync(new IdentityMessage() { Body = body, Subject = "Sitemize Hoşgeldiniz" },customer.Email);
+                }
+
+                TempData["Message"] = $"Arıza talebi onaylanmıştır";
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                TempData["Model"] = new ErrorViewModel()
+                {
+                    Text = $"Bir hata oluştu {ex.Message}",
+                    ActionName = "Index",
+                    ControllerName = "Operator",
+                    ErrorCode = 500
+                };
+                return RedirectToAction("Error", "Home");
+            }
         }
     }
 }
