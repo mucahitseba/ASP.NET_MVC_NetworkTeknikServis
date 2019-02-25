2406ec1fec54f5b30d91e736573ff79a7ebf266a
Oğuzhan DERECİ <oguzz.dereci@gmail.com>
2019-02-25 23:25:21 +0300

    anket sayfası eklendi

---
+++ NetworkTeknikServis.WEB.UI/Controllers/SurveyController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using NetworkTeknikServis.BLL.Repository;
+
+namespace NetworkTeknikServis.WEB.UI.Controllers
+{
+    public class SurveyController : Controller
+    {
+        // GET: Survey
+        public ActionResult Index(string code)
+        {
+            var fault = new FaultRepo().GetAll(x => x.SurveyCode == code).FirstOrDefault();
+
+            if (fault.AnketYapildimi == true)
+            {
+                TempData["message"] = $"{fault.FaultID} numaralı arıza için daha önceden anket yapılmıştır.";
+                return RedirectToAction("Index", "Account");
+            }
+            return View(fault);
+        }
+    }
+}
